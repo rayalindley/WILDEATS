5ad96bf77b47b3fbd3ed77cf0c46bcc33e53aa9a
Raysie <107735270+rayalindley@users.noreply.github.com>
2026-04-08 11:04:32 +0800

    Initial commit: frontend + backend setup

---
+++ wildeats-backend/src/main/java/com/example/appdev/wildeats_backend/WildeatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.appdev.wildeats_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildeatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildeatsBackendApplication.class, args);
+	}
+
+}
---
+++ wildeats-backend/src/test/java/com/example/appdev/wildeats_backend/WildeatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appdev.wildeats_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildeatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
